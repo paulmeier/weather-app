91b8b61507e3f1f97086c09a5c364fa36fd6b871
Paul Meier <paulmartinmeier@gmail.com>
2018-11-25 09:41:22 -0600

    Complete rewrite using promises.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
+require('dotenv').config();
 const yargs = require('yargs');
-const geocode = require('./geocode/geocode.js');
-const weather = require('./weather/weather.js');
-
+const axios = require('axios');
 const argv = yargs
   .options({
     address: {
@@ -15,17 +14,26 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if(errorMessage) {
-    console.log(errorMessage);
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_GEOCODE_API_KEY}&address=${encodedAddress}`;
+
+axios.get(geocodeURL).then((response) => {
+  if (response.data.status === 'ZERO_RESULTS') {
+    throw new Error('Unable to find that location.');
+  }
+  var lat = response.data.results[0].geometry.location.lat;
+  var lng = response.data.results[0].geometry.location.lng;
+  var weatherURL = `https://api.darksky.net/forecast/${process.env.FORECAST_API_KEY}/${lat},${lng}`;
+  console.log(response.data.results[0].formatted_address);
+  return axios.get(weatherURL);
+}).then((response) => {
+  var temperature = response.data.currently.temperature;
+  var apparentTemperature = response.data.currently.apparentTemperature;
+  console.log(`It's currently ${temperature}, but feels like ${apparentTemperature}`);
+}).catch((e) => {
+  if (e.code === 'ENOTFOUND') {
+    console.log('Unable to connect to API servers.');
   } else {
-    console.log(results.address);
-    weather.getTemperature(results.latitude, results.longitude, (errorMessage, weatherResults) => {
-      if(errorMessage) {
-        console.log(errorMessage);
-      } else {
-        console.log(`It's currently ${weatherResults.temperature}, but feels like ${weatherResults.apparentTemperature}`);
-      }
-    });
+    console.log(e.message);
   }
 });
